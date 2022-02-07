67c2013993b181098a068761f25fa6e64832a554
carlosmacmar <c4rlosmm96@gmail.com>
2022-02-07 11:16:28 +0100

    Fixed repositories test

--- test/features/github/data/repositories/github_repository_impl_test.dart
+++ test/features/github/data/repositories/github_repository_impl_test.dart
@@ -4,6 +4,7 @@ import 'package:dartz/dartz.dart';
 import 'package:github_api_consumer/core/error/exceptions.dart';
 import 'package:github_api_consumer/core/error/failures.dart';
 import 'package:github_api_consumer/core/network/network_info.dart';
+import 'package:github_api_consumer/core/util/enums.dart';
 import 'package:github_api_consumer/features/github/data/datasources/github_remote_data_source.dart';
 import 'package:github_api_consumer/features/github/data/models/issue_model.dart';
 import 'package:github_api_consumer/features/github/data/repositories/github_repository_impl.dart';
@@ -52,10 +53,10 @@ void main() {
       () async {
         // arrange
         when(mockNetworkInfo.isInternetAvailable).thenAnswer((_) async => true);
-        when(mockRemoteDataSource.getIssues())
+        when(mockRemoteDataSource.getIssues(1, FilterState.open, SortOption.created))
             .thenAnswer((_) async => issuesList);
         // act
-        repository.getIssues();
+        repository.getIssues(1, FilterState.open, SortOption.created);
         // assert
         verify(mockNetworkInfo.isInternetAvailable);
       },
@@ -66,12 +67,12 @@ void main() {
         'should return remote data when the call to remote data source is successful',
         () async {
           // arrange
-          when(mockRemoteDataSource.getIssues())
+          when(mockRemoteDataSource.getIssues(1, FilterState.open, SortOption.created))
               .thenAnswer((_) async => issuesList);
           // act
-          final result = await repository.getIssues();
+          final result = await repository.getIssues(1, FilterState.open, SortOption.created);
           // assert
-          verify(mockRemoteDataSource.getIssues());
+          verify(mockRemoteDataSource.getIssues(1, FilterState.open, SortOption.created));
           expect(result, equals(Right(issuesList)));
         },
       );
@@ -80,12 +81,12 @@ void main() {
         'should return server failure when the call to remote data source is unsuccessful',
         () async {
           // arrange
-          when(mockRemoteDataSource.getIssues())
+          when(mockRemoteDataSource.getIssues(1, FilterState.open, SortOption.created))
               .thenThrow(ServerException());
           // act
-          final result = await repository.getIssues();
+          final result = await repository.getIssues(1, FilterState.open, SortOption.created);
           // assert
-          verify(mockRemoteDataSource.getIssues());
+          verify(mockRemoteDataSource.getIssues(1, FilterState.open, SortOption.created));
           expect(result, equals(Left(ServerFailure())));
         },
       );
